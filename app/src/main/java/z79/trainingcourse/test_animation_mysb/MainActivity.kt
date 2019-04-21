0e1681db4120f06625ded2ea6520096dc9ef4919
hieupm6 <hieupm6@fsoft.com.vn>
2019-04-21 18:37:31 +0700

    push git len

---
+++ app/src/main/java/z79/trainingcourse/test_animation_mysb/MainActivity.kt
@@ -0,0 +1,12 @@
+package z79.trainingcourse.test_animation_mysb
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
